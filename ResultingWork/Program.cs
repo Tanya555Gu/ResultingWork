4855d1e82f4aba11a833b0fab1b204aa4fdd9e5c
Tatyana <tatyana@gus.ru>
2023-08-05 12:39:23 +0300

    function PrintArray

--- ResultingWork/Program.cs
+++ ResultingWork/Program.cs
@@ -28,3 +28,14 @@ string[] CreateArray()
     }
     return array;
 }
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.Write("]");
+}
